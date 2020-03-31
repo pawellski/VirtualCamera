29bd5505662ef601dd1e60613ce9c9ced157a9df
Paweł <skibaapawell@gmail.com>
2020-03-31 18:00:26 +0200

    Obsługa kilku klawiszy(RIGHT,LEFT,UP,DOWN)

--- VirtualCamera/src/controller/Controller.java
+++ VirtualCamera/src/controller/Controller.java
@@ -5,6 +5,8 @@
  */
 package controller;
 
+import camera.Camera;
+import camera.Line2D;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -17,62 +19,76 @@ import javafx.scene.paint.Color;
  */
 public class Controller {
 
+    private Camera camera;
+
     @FXML
     private Canvas viewport;
 
     public void initialize() {
         prepareBackground();
+        camera = new Camera("src/resources/cube1.txt");
+        camera.projectTo2D();
+        camera.moveConstructionToCenter();
+        draw();
     }
 
     @FXML
     public void recognizeOperation(KeyEvent k) {
-        String key = k.getCode().toString();
-        
-        switch (key) {
-            case "UP":
-                System.out.println("up");
-                break;
-            case "DOWN":
-                System.out.println("down");
-                break;
-            case "RIGHT":
-                System.out.println("right");
-                break;
-            case "LEFT":
-                System.out.println("left");
-                break;
-            case "PERIOD":
-                System.out.println(".");
-                break;
-            case "COMMA":
-                System.out.println(",");
-                break;
-            case "W":
-                System.out.println("w");
-                break;
-            case "S":
-                System.out.println("s");
-                break;
-            case "A":
-                System.out.println("a");
-                break;
-            case "D":
-                System.out.println("d");
-                break;
-            case "Z":
-                System.out.println("z");
-                break;
-            case "X":
-                System.out.println("x");
-                break;
-            case "OPEN_BRACKET":
-                System.out.println("[");
-                break;
-            case "CLOSE_BRACKET":
-                System.out.println("]");
-                break;
-            default:
-                break;
+        if (camera != null) {
+            String key = k.getCode().toString();
+            switch (key) {
+                case "UP":
+                    System.out.println("up");
+                    camera.moveUp();
+                    break;
+                case "DOWN":
+                    System.out.println("down");
+                    camera.moveDown();
+                    break;
+                case "RIGHT":
+                    System.out.println("right");
+                    camera.moveRight();
+                    break;
+                case "LEFT":
+                    System.out.println("left");
+                    camera.moveLeft();
+                    break;
+                case "PERIOD":
+                    System.out.println(".");
+                    break;
+                case "COMMA":
+                    System.out.println(",");
+                    break;
+                case "W":
+                    System.out.println("w");
+                    break;
+                case "S":
+                    System.out.println("s");
+                    break;
+                case "A":
+                    System.out.println("a");
+                    break;
+                case "D":
+                    System.out.println("d");
+                    break;
+                case "Z":
+                    System.out.println("z");
+                    break;
+                case "X":
+                    System.out.println("x");
+                    break;
+                case "OPEN_BRACKET":
+                    System.out.println("[");
+                    break;
+                case "CLOSE_BRACKET":
+                    System.out.println("]");
+                    break;
+                default:
+                    break;
+            }
+            camera.projectTo2D();
+            camera.moveConstructionToCenter();
+            draw();
         }
     }
 
@@ -90,6 +106,17 @@ public class Controller {
         gc.lineTo(1200, 600);
         gc.lineTo(0, 600);
         gc.lineTo(0, 0);
+        gc.stroke();
+    }
+
+    private void draw() {
+        prepareBackground();
+        GraphicsContext gc = viewport.getGraphicsContext2D();
+        gc.beginPath();
+        for (Line2D line : camera.getConstruction().getLines2D()) {
+            gc.moveTo(line.getPoint1().getX(), line.getPoint1().getY());
+            gc.lineTo(line.getPoint2().getX(), line.getPoint2().getY());
+        }
         gc.stroke();
     }
 
