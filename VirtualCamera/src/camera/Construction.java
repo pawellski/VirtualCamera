2d467b8b932c0bbdd62a2a508ff80babb7da4ed4
Paweł <skibaapawell@gmail.com>
2020-03-31 17:57:47 +0200

    Dodanie funkcji addLine2D i clearLines2D

--- VirtualCamera/src/camera/Construction.java
+++ VirtualCamera/src/camera/Construction.java
@@ -15,18 +15,20 @@ import java.util.List;
  * @author Paweł
  */
 public class Construction {
-    
+
     private List<Line3D> lines3D;
     private List<Line2D> lines2D;
-    
-    public Construction(){
+
+    public Construction() {
         lines3D = new ArrayList<>();
+        lines2D = new ArrayList<>();
     }
-    
-    public Construction(String adress){
+
+    public Construction(String adress) {
         try {
             ReadFile readFile = new ReadFile(adress);
             lines3D = readFile.load();
+            lines2D = new ArrayList<>();
         } catch (FileNotFoundException e) {
             System.out.println("The file was not found!");
         } catch (IOException e) {
@@ -37,9 +39,20 @@ public class Construction {
     public List<Line3D> getLines3D() {
         return lines3D;
     }
-    
-    public void addLine(Line3D line){
+
+    public List<Line2D> getLines2D() {
+        return lines2D;
+    }
+
+    public void addLine3D(Line3D line) {
         lines3D.add(line);
     }
-    
+
+    public void addLine2D(Line2D line) {
+        lines2D.add(line);
+    }
+
+    public void clearLines2D() {
+        lines2D = new ArrayList<>();
+    }
 }
