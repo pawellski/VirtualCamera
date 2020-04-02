b7c125063410dfec6d0269e30b087aab80ae9b70
Paweł <skibaapawell@gmail.com>
2020-04-02 23:39:36 +0200

    Dodanie funkcji odpowiadajacych za obroty oraz zmiana algorytmu rzutowania.

--- VirtualCamera/src/camera/Camera.java
+++ VirtualCamera/src/camera/Camera.java
@@ -11,10 +11,11 @@ package camera;
  */
 public class Camera {
 
-    private final double WIDTH = 1200.0;
-    private final double HEIGHT = 600.0;
+    private final double WIDTH = 800.0;
+    private final double HEIGHT = 800.0;
     private final double STEP = 10.0;
-    private double VPD = 500.0;
+    private final double ROTATION_ANGLE = 0.1;
+    private double focalLength = 200.0;
 
     private Construction construction;
 
@@ -52,13 +53,99 @@ public class Camera {
         }
     }
 
+    public void moveForward() {
+        for (Line3D l : construction.getLines3D()) {
+            l.getPoint1().setZ(l.getPoint1().getZ() - STEP);
+            l.getPoint2().setZ(l.getPoint2().getZ() - STEP);
+        }
+    }
+
+    public void moveBackward() {
+        for (Line3D l : construction.getLines3D()) {
+            l.getPoint1().setZ(l.getPoint1().getZ() + STEP);
+            l.getPoint2().setZ(l.getPoint2().getZ() + STEP);
+        }
+    }
+
+    public void rotateX(int sign) {
+        double y, z;
+        for (Line3D l : construction.getLines3D()) {
+            y = l.getPoint1().getY();
+            z = l.getPoint1().getZ();
+            l.getPoint1().setY(y * Math.cos(ROTATION_ANGLE * sign) - z * Math.sin(ROTATION_ANGLE * sign));
+            l.getPoint1().setZ(z * Math.cos(ROTATION_ANGLE * sign) + y * Math.sin(ROTATION_ANGLE * sign));
+            y = l.getPoint2().getY();
+            z = l.getPoint2().getZ();
+            l.getPoint2().setY(y * Math.cos(ROTATION_ANGLE * sign) - z * Math.sin(ROTATION_ANGLE * sign));
+            l.getPoint2().setZ(z * Math.cos(ROTATION_ANGLE * sign) + y * Math.sin(ROTATION_ANGLE * sign));
+        }
+    }
+
+    public void rotateY(int sign) {
+        double x, z;
+        for (Line3D l : construction.getLines3D()) {
+            x = l.getPoint1().getX();
+            z = l.getPoint1().getZ();
+            l.getPoint1().setX(x * Math.cos(ROTATION_ANGLE * sign) + z * Math.sin(ROTATION_ANGLE * sign));
+            l.getPoint1().setZ(z * Math.cos(ROTATION_ANGLE * sign) - x * Math.sin(ROTATION_ANGLE * sign));
+            x = l.getPoint2().getX();
+            z = l.getPoint2().getZ();
+            l.getPoint2().setX(x * Math.cos(ROTATION_ANGLE * sign) + z * Math.sin(ROTATION_ANGLE * sign));
+            l.getPoint2().setZ(z * Math.cos(ROTATION_ANGLE * sign) - x * Math.sin(ROTATION_ANGLE * sign));
+        }
+    }
+
+    public void rotateZ(int sign) {
+        double x, y;
+        System.out.println(Math.cos(Math.toRadians(ROTATION_ANGLE * sign)));
+        System.out.println(Math.sin(Math.toRadians(ROTATION_ANGLE * sign)));
+        for (Line3D l : construction.getLines3D()) {
+            x = l.getPoint1().getX();
+            y = l.getPoint1().getY();
+            l.getPoint1().setX(x * Math.cos(ROTATION_ANGLE * sign) - y * Math.sin(ROTATION_ANGLE * sign));
+            l.getPoint1().setY(y * Math.cos(ROTATION_ANGLE * sign) + x * Math.sin(ROTATION_ANGLE * sign));
+            x = l.getPoint2().getX();
+            y = l.getPoint2().getY();
+            l.getPoint2().setX(x * Math.cos(ROTATION_ANGLE * sign) - y * Math.sin(ROTATION_ANGLE * sign));
+            l.getPoint2().setY(y * Math.cos(ROTATION_ANGLE * sign) + x * Math.sin(ROTATION_ANGLE * sign));
+        }
+    }
+
+    public void zoomIn() {
+        focalLength += STEP;
+        if (focalLength > 1000) {
+            focalLength = 1000;
+        }
+    }
+
+    public void zoomOut() {
+        focalLength -= STEP;
+        if (focalLength < 50) {
+            focalLength = 50;
+        }
+
+    }
+
     public void projectTo2D() {
         construction.clearLines2D();
         for (Line3D l : construction.getLines3D()) {
-            double x1 = l.getPoint1().getX() / (l.getPoint1().getZ() + VPD) * VPD;
-            double y1 = l.getPoint1().getY() / (l.getPoint1().getZ() + VPD) * VPD;
-            double x2 = l.getPoint2().getX() / (l.getPoint2().getZ() + VPD) * VPD;
-            double y2 = l.getPoint2().getY() / (l.getPoint2().getZ() + VPD) * VPD;
+            double x1, x2, y1, y2;
+            double z1 = l.getPoint1().getZ();
+            double z2 = l.getPoint2().getZ();
+            if (z1 > 1) {
+                x1 = l.getPoint1().getX() * focalLength / z1;
+                y1 = l.getPoint1().getY() * focalLength / z1;
+            } else {
+                x1 = l.getPoint1().getX() * focalLength;
+                y1 = l.getPoint1().getY() * focalLength;
+            }
+            if (z2 > 1) {
+                x2 = l.getPoint2().getX() * focalLength / z2;
+                y2 = l.getPoint2().getY() * focalLength / z2;
+            } else {
+                x2 = l.getPoint2().getX() * focalLength;
+                y2 = l.getPoint2().getY() * focalLength;
+            }
             construction.addLine2D(new Line2D(new Point2D(x1, y1), new Point2D(x2, y2)));
         }
     }
